eca5a79a63357ee3e4bc0aaa8620b05f008aae8a
Cameron Warner <cameron.w.warner@gmail.vom>
2023-03-27 09:42:21 -0400

    Initial Commit

---
+++ Planted/PlantedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantedApp.swift
+//  Planted
+//
+//  Created by Cameron Warner on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
